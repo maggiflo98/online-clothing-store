c080da88cd995263e6049bb01f510c6659a6014a
maggiflo98 <maggiflo98@gmail.com>
2021-07-24 06:48:14 +0300

    added menu and directry component with styles

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HomePage from "./pages/homepage/homepage.components";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+export default App;
---
+++ src/pages/homepage/homepage.components.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => (
+  <div className="HomePage">
+    <Directory />
+  </div>
+);
+// <div className="homepage">
+//   <div className="directory-menu">
+//     <div className="menu-item">
+//       <div className="content">
+//         <h1 className="title">HATS</h1>
+//         <span className="sub-title">SHOP NOW</span>
+//       </div>
+//     </div>
+//     {/*hats  */}
+//     <div className="menu-item">
+//       <div className="content">
+//         <h1 className="title">JACKETS</h1>
+//         <span className="sub-title">SHOP NOW</span>
+//       </div>
+//     </div>
+//     {/* jackets */}
+//     <div className="menu-item">
+//       <div className="content">
+//         <h1 className="title">SNEAKERS</h1>
+//         <span className="sub-title">SHOP NOW</span>
+//       </div>
+//     </div>
+//     {/* sneakers */}
+//     <div className="menu-item">
+//       <div className="content">
+//         <h1 className="title">WOMENS</h1>
+//         <span className="sub-title">SHOP NOW</span>
+//       </div>
+//     </div>
+//     {/* WOMENS */}
+
+//     <div className="menu-item">
+//       <div className="content">
+//         <h1 className="title">MENS</h1>
+//         <span className="sub-title">SHOP NOW</span>
+//       </div>
+//     </div>
+//     {/* mens */}
+//   </div>
+// </div>
+
+export default HomePage;
