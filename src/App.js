0967b00f80a3e6751a193a46bf7d9ef6e7f9138d
maggiflo98 <maggiflo98@gmail.com>
2021-08-06 09:45:58 +0300

    update our men-item to navigate with RouterHoc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.components";
+
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+      {/* switch will render homepage frst and not anthng  else */}
+
+      {/* <HomePage /> */}
     </div>
   );
 }
